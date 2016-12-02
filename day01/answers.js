b3257e0c8977315f33ef98c87b9aca44736d8d3b
ksmithut <ksmith.ut@gmail.com>
2016-12-02 13:45:07 -0700

    solves day 1

--- day01/answers.js
+++ day01/answers.js
@@ -1,15 +1,17 @@
 'use strict'
 
 exports.part1 = {
-  answer: null,
+  answer: 234,
   examples: [
-    { input: '', value: null },
+    { input: 'R2, L3', value: 5 },
+    { input: 'R2, R2, R2', value: 2 },
+    { input: 'R5, L5, R5, R3', value: 12 },
   ]
 }
 
 exports.part2 = {
-  answer: null,
+  answer: 113,
   examples: [
-    { input: '', value: null },
+    { input: 'R8, R4, R4, R8', value: 4 },
   ]
 }
