8b142fdfd7ab0d2ede2c3a34a5ab44ede29b6ff0
ksmithut <ksmith.ut@gmail.com>
2016-12-03 09:12:08 -0700

    day3

--- day03/answers.js
+++ day03/answers.js
@@ -1,15 +1,25 @@
 'use strict'
 
 exports.part1 = {
-  answer: null,
+  answer: 993,
   examples: [
-    { input: '', value: null },
+    { input: ' 5  10  25 ', value: 0 },
   ]
 }
 
 exports.part2 = {
-  answer: null,
+  answer: 1849,
   examples: [
-    { input: '', value: null },
+    {
+      input: [
+        '101 301 501',
+        '102 302 502',
+        '103 303 503',
+        '201 401 601',
+        '202 402 602',
+        '203 403 603',
+      ].join('\n'),
+      value: 6,
+    },
   ]
 }
