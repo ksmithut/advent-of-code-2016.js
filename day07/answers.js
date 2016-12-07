eec456bc631577c61df23f35935fa74f8a9a2953
ksmithut <ksmith.ut@gmail.com>
2016-12-07 09:08:10 -0700

    adds day7

--- day07/answers.js
+++ day07/answers.js
@@ -1,15 +1,21 @@
 'use strict'
 
 exports.part1 = {
-  answer: null,
+  answer: 115,
   examples: [
-    { input: '', value: null },
+    { input: 'abba[mnop]qrst', value: 1 },
+    { input: 'abcd[bddb]xyyx', value: 0 },
+    { input: 'aaaa[qwer]tyui', value: 0 },
+    { input: 'ioxxoj[asdfgh]zxcvbn', value: 1 },
   ],
 }
 
 exports.part2 = {
-  answer: null,
+  answer: 231,
   examples: [
-    { input: '', value: null },
+    { input: 'aba[bab]xyz', value: 1 },
+    { input: 'xyx[xyx]xyx', value: 0 },
+    { input: 'aaa[kek]eke', value: 1 },
+    { input: 'zazbz[bzb]cdb', value: 1 },
   ],
 }
